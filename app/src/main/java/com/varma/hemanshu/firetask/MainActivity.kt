cafc3d67224e612ead9e23a3e9435029e57c7dbe
Hemanshu Varma <hemanshuvarma916@gmail.com>
2019-10-18 17:54:49 +0530

    Add Firebase Auth UI

--- app/src/main/java/com/varma/hemanshu/firetask/MainActivity.kt
+++ app/src/main/java/com/varma/hemanshu/firetask/MainActivity.kt
@@ -1,12 +1,85 @@
 package com.varma.hemanshu.firetask
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    val RC_SIGN_IN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showLoginUI()
+    }
+
+    private fun showLoginUI() {
+        // Auth UI providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.signOut -> {
+                signOutUser()
+            }
+        }
+        return true
+    }
+
+    private fun signOutUser() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                Toast.makeText(applicationContext, "Signed out successfully", Toast.LENGTH_SHORT)
+                    .show()
+                showLoginUI()
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(applicationContext, "Sign In Success", Toast.LENGTH_SHORT).show()
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                Toast.makeText(applicationContext, "Sign in Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
